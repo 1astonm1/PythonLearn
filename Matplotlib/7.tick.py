87bd8ad425b4808a0ebe8808cfb64e62c7ad9e55
astonm <45306215+1astonm1@users.noreply.github.com>
2019-01-30 22:28:44 +0800

    2019.1.30.22.28

--- Matplotlib/7.tick.py
+++ Matplotlib/7.tick.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+# 修改图层顺序，防止遮住坐标
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,7 +16,7 @@ ax.spines['top'].set_color("none")
 ax.spines['right'].set_color("none")
 ax.spines['bottom'].set_position(("data", 0))
 ax.spines['left'].set_position(("data", 0))
-##此时图像遮住了坐标轴
+# 此时图像遮住了坐标轴
 for label in ax.get_xticklabels() + ax.get_yticklabels():
     label.set_fontsize(12)
     # 在 plt 2.0.2 或更高的版本中, 设置 zorder 给 plot 在 z 轴方向排序
