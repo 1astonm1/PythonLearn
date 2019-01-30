8bf7dfc7917f9f976d7da002dd8503147bb02d69
astonm <45306215+1astonm1@users.noreply.github.com>
2019-01-30 22:27:30 +0800

    2019.1.30.22.27

--- Matplotlib/5.legend.py
+++ Matplotlib/5.legend.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-
+# 给图加说明标签
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,19 +9,19 @@ y1 = 2*x + 1
 y2 = x**2
 
 plt.figure()
-#加标签
+# 加标签
 plt.plot(x, y1, color='blue', label='square line')
 plt.plot(x, y2, color='red', linestyle='--', label='linear line')
-plt.legend(loc='best')   #upper right      upper left	   lower left	   lower right
+plt.legend(loc='best')   # upper right      upper left	   lower left	   lower right
                                     # right	center   left	  center right	    lower center	   upper center	   center
-                                    #best自动分配最好的位置
+                                    # best自动分配最好的位置
 
 plt.xlim(-5, 5)
 plt.ylim(-5, 5)
 plt.xlabel("this is x")
 plt.ylabel("this is y")
 
-#设置坐标轴
+# 设置坐标轴
 ax = plt.gca()
 ax.spines['right'].set_color("none")
 ax.spines['top'].set_color("none")
