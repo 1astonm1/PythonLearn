06a8ba6f51a67711feb39528ca4752b2502c061e
astonm <45306215+1astonm1@users.noreply.github.com>
2019-01-30 22:26:29 +0800

    2019.1.30.22.26
    备注修改

--- Matplotlib/4.annotation.py
+++ Matplotlib/4.annotation.py
@@ -21,12 +21,12 @@ ax.spines['left'].set_position(("data", 0))
 
 plt.xlim(-3, 3)
 plt.ylim(-6, 6)
-#画点和虚线
+# 画点和虚线
 x0 = 1
 y0 = 2*x0 + 1
 plt.scatter(x0, y0, s=50, color='black')
 plt.plot([x0, x0], [y0, 0], color='black', linestyle='--', lw=2.5)
-#annotate使用说明 做标注
+# annotate使用说明 做标注
 plt.annotate(r'$2x+1=%s$' % y0, xy=(x0, y0), xycoords='data', xytext=(+30, -30),
                     textcoords='offset points', fontsize=16,
                     arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.2'))
