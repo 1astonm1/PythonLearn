e49af4c820d5053b28b2d73339c650436094d867
astonm <45306215+1astonm1@users.noreply.github.com>
2019-03-14 11:04:42 +0800

    2019.3.14.11.04

--- Pandas/5.concat.py
+++ Pandas/5.concat.py
@@ -3,10 +3,14 @@
 import numpy as np
 import pandas as pd
 
+#######################################################
+# axis合并 axis=0纵向合并
+#######################################################
+
 # 定义资料集
-df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
-df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
-df3 = pd.DataFrame(np.ones((3,4))*2, columns=['a','b','c','d'])
+df1 = pd.DataFrame(np.ones((3, 4))*0, columns=['a', 'b', 'c', 'd'])
+df2 = pd.DataFrame(np.ones((3, 4))*1, columns=['a', 'b', 'c', 'd'])
+df3 = pd.DataFrame(np.ones((3, 4))*2, columns=['a', 'b', 'c', 'd'])
 
 # concat纵向合并
 res = pd.concat([df1, df2, df3], axis=0)
@@ -24,6 +28,10 @@ print(res)
 # 1  2.0  2.0  2.0  2.0
 # 2  2.0  2.0  2.0  2.0
 
+
+#######################################################
+# 重置index 合并后index是混乱的，加上这个可以直接更新index
+#######################################################
 # 承上一个例子，并将index_ignore设定为True
 res = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
 
@@ -41,3 +49,119 @@ print(res)
 # 8  2.0  2.0  2.0  2.0
 
 
+#######################################################
+# join方式 outer为预设值 原本没有的位置用nan填充
+#######################################################
+# 定义资料集
+df1 = pd.DataFrame(np.ones((3, 4))*0, columns=['a', 'b', 'c', 'd'], index=[1, 2, 3])
+df2 = pd.DataFrame(np.ones((3, 4))*1, columns=['b', 'c', 'd', 'e'], index=[2, 3, 4])
+
+# 纵向"外"合并df1与df2
+res = pd.concat([df1, df2], axis=0, join='outer')
+
+print(res)
+#     a    b    c    d    e
+# 1  0.0  0.0  0.0  0.0  NaN
+# 2  0.0  0.0  0.0  0.0  NaN
+# 3  0.0  0.0  0.0  0.0  NaN
+# 2  NaN  1.0  1.0  1.0  1.0
+# 3  NaN  1.0  1.0  1.0  1.0
+# 4  NaN  1.0  1.0  1.0  1.0
+
+#######################################################
+# join方式 inner为内合并
+#######################################################
+# 承上一个例子
+
+# 纵向"内"合并df1与df2
+res = pd.concat([df1, df2], axis=0, join='inner')
+
+# 打印结果
+print(res)
+#     b    c    d
+# 1  0.0  0.0  0.0
+# 2  0.0  0.0  0.0
+# 3  0.0  0.0  0.0
+# 2  1.0  1.0  1.0
+# 3  1.0  1.0  1.0
+# 4  1.0  1.0  1.0
+
+# 重置index并打印结果
+res = pd.concat([df1, df2], axis=0, join='inner', ignore_index=True)
+print(res)
+#     b    c    d
+# 0  0.0  0.0  0.0
+# 1  0.0  0.0  0.0
+# 2  0.0  0.0  0.0
+# 3  1.0  1.0  1.0
+# 4  1.0  1.0  1.0
+# 5  1.0  1.0  1.0
+
+#######################################################
+# join_axes 按照某一个的坐标合并
+#######################################################
+# 定义资料集
+df1 = pd.DataFrame(np.ones((3, 4))*0, columns=['a', 'b', 'c', 'd'], index=[1, 2, 3])
+df2 = pd.DataFrame(np.ones((3, 4))*1, columns=['b', 'c', 'd', 'e'], index=[2, 3, 4])
+
+# 依照`df1.index`进行横向合并
+res = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
+
+# 打印结果
+print(res)
+#     a    b    c    d    b    c    d    e
+# 1  0.0  0.0  0.0  0.0  NaN  NaN  NaN  NaN
+# 2  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+# 3  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+
+# 移除join_axes，并打印结果
+res = pd.concat([df1, df2], axis=1)
+print(res)
+#     a    b    c    d    b    c    d    e
+# 1  0.0  0.0  0.0  0.0  NaN  NaN  NaN  NaN
+# 2  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+# 3  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+# 4  NaN  NaN  NaN  NaN  1.0  1.0  1.0  1.0
+
+#######################################################
+# append添加数据
+#######################################################
+# 定义资料集
+df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
+df3 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
+s1 = pd.Series([1,2,3,4], index=['a','b','c','d'])
+
+# 将df2合并到df1的下面，以及重置index，并打印出结果
+res = df1.append(df2, ignore_index=True)
+print(res)
+#     a    b    c    d
+# 0  0.0  0.0  0.0  0.0
+# 1  0.0  0.0  0.0  0.0
+# 2  0.0  0.0  0.0  0.0
+# 3  1.0  1.0  1.0  1.0
+# 4  1.0  1.0  1.0  1.0
+# 5  1.0  1.0  1.0  1.0
+
+# 合并多个df，将df2与df3合并至df1的下面，以及重置index，并打印出结果
+res = df1.append([df2, df3], ignore_index=True)
+print(res)
+#     a    b    c    d
+# 0  0.0  0.0  0.0  0.0
+# 1  0.0  0.0  0.0  0.0
+# 2  0.0  0.0  0.0  0.0
+# 3  1.0  1.0  1.0  1.0
+# 4  1.0  1.0  1.0  1.0
+# 5  1.0  1.0  1.0  1.0
+# 6  1.0  1.0  1.0  1.0
+# 7  1.0  1.0  1.0  1.0
+# 8  1.0  1.0  1.0  1.0
+
+# 合并series，将s1合并至df1，以及重置index，并打印出结果
+res = df1.append(s1, ignore_index=True)
+print(res)
+#     a    b    c    d
+# 0  0.0  0.0  0.0  0.0
+# 1  0.0  0.0  0.0  0.0
+# 2  0.0  0.0  0.0  0.0
+# 3  1.0  2.0  3.0  4.0
