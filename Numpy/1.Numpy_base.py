57a19c5037853fa28374e11a99b2a8ed98dd37e7
astonm <45306215+1astonm1@users.noreply.github.com>
2019-03-19 20:55:50 +0800

    2019.3.19.20.55

--- Numpy/1.Numpy_base.py
+++ Numpy/1.Numpy_base.py
@@ -2,19 +2,34 @@
 
 import numpy as np
 
-#列表转化为矩阵
+# 列表转化为矩阵
 array = np.array([[1, 2, 3], [2, 3, 4]])
 print(array)
 
-#基本特征
-print(array.ndim)#维度
-print(array.shape)#行数和列数
-print(array.shape[0])#只输出行数
-print(array.shape[1])#只输出列数
-print(array.size)#元素个数
+# [[1 2 3]
+#  [2 3 4]]
+
+# 基本特征
+print(array.ndim)   # 维度
+print(array.shape)  # 行数和列数
+print(array.shape[0])   # 只输出行数
+print(array.shape[1])   # 只输出列数
+print(array.size)   # 元素个数
+
+# 2
+# (2, 3)
+# 2
+# 3
+# 6
 
 for i in range(0, 2, 1):
     for j in range(0, 3, 1):
-        print(("第"+str(i+1)+"行，第"+str(j+1)+"列的数值是：")+str(array[i][j]))#可以像二维数组一样调用array中的值
+        print(("第"+str(i+1)+"行，第"+str(j+1)+"列的数值是：")+str(array[i][j]))  # 可以像二维数组一样调用array中的值
 
-
+#
+# 第1行，第1列的数值是：1
+# 第1行，第2列的数值是：2
+# 第1行，第3列的数值是：3
+# 第2行，第1列的数值是：2
+# 第2行，第2列的数值是：3
+# 第2行，第3列的数值是：4
