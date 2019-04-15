94e93d38e13bfc48093707bd6aeeed954d2e3e4b
astonm <45306215+1astonm1@users.noreply.github.com>
2019-04-15 16:44:32 +0800

    2019.4.15.16.44

--- PythonBase/核心模块/os/os_path.py
+++ PythonBase/核心模块/os/os_path.py
@@ -1,6 +1,16 @@
-from os import *
+import os
 
 path = os.getcwd()
-os.mkdir(path=path+"test")
+os.mkdir(path=path+"/test")
+
+os.path.exists(path)    # 判断当前path对应文件或者目录是否存在，返回Tru或者False
+os.path.isfile(path)    # 判断path是否是已经存在的文件
+os.path.isdir(path)     # 判断path是否是已经存在的目录
+os.path.getatime(path)  # 返回path对应文件或目录上一次的访问时间
+
+os.path.getmtime(path)  # 返回path对应文件或目录上一次的修改时间
+os.path.getctime(path)  # 返回path对应文件或目录的创建时间
+
+os.path.getsize(path)   # 返回path对应文件的大小，以字节为单位
 
 
